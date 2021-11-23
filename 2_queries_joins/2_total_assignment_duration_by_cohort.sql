25f068cc562f1e0ed5b5185cf1ec7d456622c360
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-11-23 05:29:33 +0000

    Added queries with GROUP and HAVING clauses

--- 2_queries_joins/2_total_assignment_duration_by_cohort.sql
+++ 2_queries_joins/2_total_assignment_duration_by_cohort.sql
@@ -2,4 +2,4 @@ SELECT SUM(duration) as total_duration
 FROM assignment_submissions 
 INNER JOIN students ON student_id = students.id
 INNER JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = 'FEB12'; 
+WHERE cohorts.name = 'FEB12';  
---
+++ 3_queries_group-by/4_total_cohort_submissions.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) as total_submissions
+FROM cohorts
+INNER JOIN students ON cohorts.id = cohort_id
+INNER JOIN assignment_submissions ON students.id = student_id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
