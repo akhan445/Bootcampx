ccc773a1fb57596f5750bd8d7d314aacc0cf58cc
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-11-25 01:10:00 +0000

    Changed code to protect from malicious injection attacks

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,15 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON assistance_requests.student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+  WHERE cohorts.name = $1
   ORDER BY teachers.name;
-`)
+`;
+
+const cohortName = process.argv[2] || 'JUL02';
+
+const values = [cohortName];
+
+pool.query(queryString, values)
 .then(res => {
   // console.log(res)
   res.rows.forEach(user => {
